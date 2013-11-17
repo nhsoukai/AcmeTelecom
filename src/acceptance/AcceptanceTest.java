d57d6e6c8854445738971e6a90550e092a84f021
andreishappy <andreishappy@gmail.com>
2013-11-17 14:06:54 +0000

    Acceptance Test Written with Mock of Tariff. Added and expected text file which is compared against the actual output of the system

--- src/acceptance/AcceptanceTest.java
+++ src/acceptance/AcceptanceTest.java
@@ -4,25 +4,28 @@ import com.acmetelecom.BillGenerator;
 import com.acmetelecom.BillingSystemTestDummy;
 import com.acmetelecom.CallEnd;
 import com.acmetelecom.CallStart;
-import com.acmetelecom.customer.Customer;
-import com.acmetelecom.customer.CustomerDatabase;
-import com.acmetelecom.customer.TariffLibrary;
+import com.acmetelecom.customer.*;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
 import org.jmock.integration.junit4.JUnit4Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
 import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
+import jdave.unfinalizer.Unfinalizer;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created with IntelliJ IDEA.
@@ -33,21 +36,31 @@ import java.util.List;
  */
 @RunWith(JMock.class)
 public class AcceptanceTest {
-    Mockery context = new JUnit4Mockery();
-    TariffLibrary tariffLibrary = context.mock(TariffLibrary.class);
-    CustomerDatabase customerDatabase = context.mock(CustomerDatabase.class);
+
+    private Mockery context  = new JUnit4Mockery() {{
+        setImposteriser(ClassImposteriser.INSTANCE);
+    }};;
+    //Mockery context = new JUnit4Mockery();
+    TariffLibrary tariffLibrary;
+    CustomerDatabase customerDatabase;
 
     //Mocked everything but the billGenerator, which is part of the system
-    BillingSystemTestDummy billingSystem =
-            new BillingSystemTestDummy(tariffLibrary,
-                                       customerDatabase,
-                                       new BillGenerator());
+    BillingSystemTestDummy billingSystem;
 
-    PrintStream outputStream = new PrintStream(new ByteArrayOutputStream());
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream printer = new PrintStream(outputStream);
     PrintStream stdout = System.out;
     @Before
     public void initOutputStream() {
-        System.setOut(outputStream);
+        System.setOut(printer);
+
+        Unfinalizer.unfinalize();
+
+        tariffLibrary = context.mock(TariffLibrary.class);
+        customerDatabase = context.mock(CustomerDatabase.class);
+        billingSystem  = new BillingSystemTestDummy(tariffLibrary,
+                customerDatabase,
+                new BillGenerator());
     }
 
     @After
@@ -56,32 +69,120 @@ public class AcceptanceTest {
     }
 
     @Test
-    public void acceptanceTest() {
-        final CallStart start = new CallStart("07920445573","2",new DateTime(2013,12,12,21,30,10));
-        final CallEnd end = new CallEnd("07920445573","2",new DateTime(2013,12,12,21,31,12));
+    public void acceptanceTest() throws IOException {
+        //Andrei Calls
+        //Call before the peak period
+        final DateTime startTime1 = new DateTime(2013,12,12,5,0);
+        final CallStart startOffPeakA = new CallStart("0771","0772",startTime1);
+        final DateTime endTime1 = new DateTime(2013, 12, 12, 5,10);
+        final CallEnd endOffPeakA = new CallEnd("0771","0772",  endTime1);
+        //Call that starts before and ends in peak time
+        final DateTime startTime2 = new DateTime(2013, 12, 12, 6, 50);
+        final CallStart startBothA= new CallStart("0771","0772",  startTime2);
+        final DateTime endTime2 = new DateTime(2013, 12, 12, 7, 10);
+        final CallEnd endBothA = new CallEnd("0771","0772", endTime2);
+        //Call that is all in peak time
+        final DateTime startTime3 = new DateTime(2013, 12, 12, 8, 0);
+        final CallStart startPeakA = new CallStart("0771","0772", startTime3);
+        final DateTime endTime3 = new DateTime(2013, 12, 12, 8, 10);
+        final CallEnd endPeakA = new CallEnd("0771","0772",  endTime3);
+
+        //Bianca Calls
+        //Call that starts in the peak period and ends after
+        final DateTime startTime4 = new DateTime(2013, 12, 12, 18, 50);
+        final CallStart startBothB = new CallStart("0772","0771", startTime4);
+        final DateTime endTime4 = new DateTime(2013, 12, 12, 19, 10);
+        final CallEnd endBothB = new CallEnd("0772","0771", endTime4);
+        //Call that is all after peak time
+        final DateTime startTime5 = new DateTime(2013, 12, 12, 20, 0);
+        final CallStart startOffPeakB = new CallStart("0772","0771",  startTime5);
+        final DateTime endTime5 = new DateTime(2013, 12, 12, 20, 10);
+        final CallEnd endOffPeakB = new CallEnd("0772","0771", endTime5);
+        //Call that starts before peak period and ends after
+        final DateTime startTime6 = new DateTime(2013,12,12,6,00);
+        final DateTime endTime6 = new DateTime(2013,12,12,20,00);
+        final CallStart startLongB = new CallStart("0772","0771",startTime6);
+        final CallEnd endLongB = new CallEnd("0772","0771",endTime6);
+
         final List<Customer> customerList = new ArrayList<Customer>();
-        final Customer andrei = context.mock(Customer.class);
-        //final Customer andrei = new Customer("07920445573","Andrei","Standard");
+        final Customer andrei = new Customer("Andrei", "0771", "Standard");
+        final Customer bianca = new Customer("Bianca","0772", "Expensive");
         customerList.add(andrei);
-        final BigDecimal offpeakTariff = new BigDecimal(1);
-        final TariffMock tariff = context.mock(TariffMock.class);
+        customerList.add(bianca);
+
+        //Tariffs
+        final BigDecimal offPeakTariffAndrei = new BigDecimal("0.03");
+        final BigDecimal peakTariffAndrei = new BigDecimal("0.06");
+        final BigDecimal offPeakTariffBianca = new BigDecimal("0.04");
+        final BigDecimal peakTariffBianca = new BigDecimal("0.08");
+
+        //final TariffMock tariffAndrei = context.mock(TariffMock.class,"AndreiTariffMock");
+        //final TariffMock tariffBianca = context.mock(TariffMock.class,"BiancaTariffMock");
+        context.setImposteriser(ClassImposteriser.INSTANCE);
+
+        final Tariff tariffAndrei = context.mock(Tariff.class,"tariffAndrei");
+        final Tariff tariffBianca = context.mock(Tariff.class,"tariffBianca");
 
         context.checking( new Expectations() {{
             allowing(customerDatabase).getCustomers(); will(returnValue(customerList));
-            allowing(tariffLibrary).tarriffFor(andrei); will(returnValue(tariff));
-            allowing(tariff).offPeakRate(); will(returnValue(offpeakTariff));
+            allowing(tariffLibrary).tarriffFor(andrei); will(returnValue(tariffAndrei));
+            allowing(tariffLibrary).tarriffFor(bianca); will(returnValue(tariffBianca));
+            allowing(tariffAndrei).peakRate(); will(returnValue(peakTariffAndrei));
+            allowing(tariffAndrei).offPeakRate(); will(returnValue(offPeakTariffAndrei));
+            allowing(tariffBianca).peakRate(); will(returnValue(peakTariffBianca));
+            allowing(tariffBianca).offPeakRate(); will(returnValue(offPeakTariffBianca));
 
 
-
-
-
-       }
+        }
         });
+        //AndreiCalls
+        billingSystem.callInitiatedAt("0771","0772",startTime1);
+        billingSystem.callCompletedAt("0771","0772",endTime1);
+        billingSystem.callInitiatedAt("0771","0772",startTime2);
+        billingSystem.callCompletedAt("0771","0772",endTime2);
+        billingSystem.callInitiatedAt("0771","0772",startTime3);
+        billingSystem.callCompletedAt("0771","0772",endTime3);
+        //BiancaCalls
+        billingSystem.callInitiatedAt("0772","0771",startTime4);
+        billingSystem.callCompletedAt("0772","0771",endTime4);
+        billingSystem.callInitiatedAt("0772","0771",startTime5);
+        billingSystem.callCompletedAt("0772","0771",endTime5);
+        billingSystem.callInitiatedAt("0772","0771",startTime6);
+        billingSystem.callCompletedAt("0772","0771",endTime6);
 
-        String st = outputStream.toString();
+        billingSystem.createCustomerBills();
+
+        System.out.flush();
+        String outputStreamString = outputStream.toString();
+
+        String[] outputLinesArray = outputStreamString.split("\n");
+
+        ArrayList<String> outputLinesArrayList = new ArrayList<String>(Arrays.asList(outputLinesArray));
+        Iterator<String> outputLineIterator = outputLinesArrayList.iterator();
+
+        BufferedReader br = new BufferedReader(new FileReader("expected"));
+
+        String expected, outputLine, message;
+        int lineNumber = 1;
+
+        while ((expected = br.readLine()) != null) {
+            outputLine = outputLineIterator.next();
+            StringBuilder st = new StringBuilder();
+            st.append("On line ");
+            st.append(lineNumber);
+            st.append("%n   EXPECTED: ");
+            st.append(expected);
+            st.append("%n   ACTUAL: ");
+            st.append(outputLine);
+            message = st.toString();
+            assertEquals(expected,outputLine);
+
+            // process the line.
+        }
+        br.close();
+
+
         System.setOut(stdout);
-
-        System.out.println(st);
 
     }
 
@@ -89,3 +190,4 @@ public class AcceptanceTest {
 
 
 }
+
