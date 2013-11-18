771935a3b1b7406da4bb37d15e124669e70d1303
soukaina <nhsoukaina@gmail.com>
2013-11-18 17:36:39 +0000

    fixed it

--- src/com/acmetelecom/DayTimePeakPeriodTest.java
+++ src/com/acmetelecom/DayTimePeakPeriodTest.java
@@ -8,7 +8,9 @@ package com.acmetelecom;
  * To change this template use File | Settings | File Templates.
  */
 import org.joda.time.MutableDateTime;
+import org.joda.time.*;
 import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
 
 public class DayTimePeakPeriodTest {
@@ -17,6 +19,20 @@ public class DayTimePeakPeriodTest {
     MutableDateTime start=new MutableDateTime();
     MutableDateTime end=new MutableDateTime();
 
+    @Test
+    public void startOverlapPeakTime() {
+        start.setTime(6,50,0,0);
+        end.setTime(7,10,0,0);
+
+        CallStart callStart= new CallStart("447722113434", "447766511332", new DateTime(2013,12,12,6,50));
+        CallEnd callEnd= new CallEnd("447722113434", "447766511332", new DateTime(2013,12,12,7,10));
+        //end.toDateTime());
+        Call call=new Call(callStart,callEnd);
+
+        assertEquals(600, DaytimePeakPeriod.offPeakDuration(call));
+        assertEquals(600, DaytimePeakPeriod.peakDuration(call));
+
+    }
 
     @Test
     public void overlapPeakTime(){
--- src/com/acmetelecom/DaytimePeakPeriod.java
+++ src/com/acmetelecom/DaytimePeakPeriod.java
@@ -6,10 +6,10 @@ import org.joda.time.MutableDateTime;
 class DaytimePeakPeriod {
 
 
-    static public Interval initializePeak(){
-        MutableDateTime peak1= new MutableDateTime();
+    static public Interval initializePeak(DateTime day){
+        MutableDateTime peak1=day.toMutableDateTime();
+        MutableDateTime peak2= day.toMutableDateTime();
         peak1.setTime(7,0,0,0);
-        MutableDateTime peak2= new MutableDateTime();
         peak2.setTime(19,0,0,0);
         return new Interval(peak1,peak2);
     }
@@ -32,13 +32,14 @@ class DaytimePeakPeriod {
         DateTime startTime=call.startTime();
         DateTime endTime=call.endTime();
         Duration callInterval= new Duration(startTime,endTime);
-        if (callInterval.minus(peakDuration(call))==null) return 0;
+        if (callInterval.minus(peakDuration(call)*1000)==null) return 0;
         else
         return callInterval.minus(peakDuration(call)*1000).getStandardSeconds();
     }
     static public long peakDuration(Call call) {
-        Interval peak= initializePeak();
+
         DateTime startTime=call.startTime();
+        Interval peak= initializePeak(startTime);
         DateTime endTime=call.endTime();
         Interval callInterval= new Interval(startTime,endTime);
         Interval peakInterval = callInterval.overlap(peak);
